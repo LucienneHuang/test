f2673a4912646d610c745f2eca62957adc5ef1fa
lucienne <lucienne>
2026-02-11 10:27:42 +0800

    增加測試

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -2,6 +2,13 @@
 </script>
 <template>
     header
+    <div>
+        <NuxtLink to="/">首頁</NuxtLink>
+        <NuxtLink to="/About">關於我們</NuxtLink>
+        <NuxtLink to="/blog">blog</NuxtLink>
+        <NuxtLink to="/blog/intro">blog 介紹</NuxtLink>
+        <NuxtLink to="/branch">branch</NuxtLink>
+    </div>
 </template>
 <style scoped>
 </style>
